5c4e592bb1269ee65f1e5efc46e05d02218077f5
yeeun426 <thsudkcla7@naver.com>
2023-06-13 02:55:52 +0900

    refactor: 코드리팩토링

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -8,6 +8,8 @@ import SwiperCore, { Navigation, Autoplay } from "swiper";
 import 'swiper/css';
 import "swiper/css/navigation";
 
+import MainData from "../data/MainData.json"
+
 export default function MainPage() {
 
   SwiperCore.use([Navigation, Autoplay])	
@@ -28,63 +30,26 @@ export default function MainPage() {
           modules={[Autoplay, Navigation]}
           className="main-banner"
         >      
-          <SwiperSlide>
-            <img src = "images/banner1.png" alt="산업재해"/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src = "images/banner2.png" alt="신청절차"/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src = "images/banner3.png" alt="커뮤니티"/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src = "images/banner4.png" alt="process"/>
-          </SwiperSlide>
-          <SwiperSlide>
-            <img src = "images/banner5.png" alt="process"/>
-          </SwiperSlide>
+        {MainData.swiper.map((swiper) => (
+          <SwiperSlide key={swiper.id}>
+            <Link to = {swiper.link}>
+            <img src = {swiper.img} alt={swiper.alt}/>
+            </Link>
+          </SwiperSlide>       
+        ))}
         </Swiper>
-
         <div className = "main-services">
           <div>NDGD가 제공하는 서비스</div>
           
           <div className = "main-service-btn">
-            <Link to = "/procedure">
+          {MainData.mainBtn.map((btn) => (
+            <Link to = {btn.link} key={btn.id}>
               <button className = "main-service-items">
-                <img src = "images/process.png" alt="process"/>
-                <div>산재보상절차/신청</div>
+                <img src = {btn.img} alt={btn.alt}/>
+                <div>{btn.txt}</div>
               </button>
             </Link>
-            <Link to = "/calc">
-              <button className = "main-service-items">
-                <img src = "images/calculator.png" alt="process"/>
-                <div>4대 보험금 모의계산</div>
-              </button>
-            </Link>
-            <Link to = "/percent">
-              <button className = "main-service-items">
-                <img src = "images/percentage.png" alt="process"/>
-                <div>보험금 승인 확률</div>
-              </button>
-            </Link>
-            <Link to = "/training">
-              <button className = "main-service-items">
-                <img src = "images/best.png" alt="process"/>
-                <div>직업훈련 우수 기관</div>
-              </button>
-            </Link>
-            <Link to = "/medical">
-              <button className = "main-service-items">
-                <img src = "images/medical.png" alt="process"/>
-                <div>산재 지정 의료 기관</div>
-              </button>
-            </Link>
-            <Link to = "/rehabilitation">
-              <button className = "main-service-items">
-                <img src = "images/job.png" alt="process"/>
-                <div>직업 재활 기관 현황</div>
-              </button>
-            </Link>
+          ))}
           </div>
         </div>
     </MainStyled>
