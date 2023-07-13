6055a4d80b4305d275913a0e84d59e4338bf9f2e
yeeun426 <thsudkcla7@naver.com>
2023-07-13 13:29:45 +0900

    feat : kakaomap 마커 추가

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import jobData from "../data/JobInstitutionData.json"
 
 const { kakao } = window;
 
@@ -7,10 +8,17 @@ const MapContainer = () => {
     useEffect(() => {
         const container = document.getElementById('myMap');
 		const options = {
-			center: new kakao.maps.LatLng(37, 127.5),
+			center: new kakao.maps.LatLng(36.3, 127.5),
 			level: 12
 		};
         const map = new kakao.maps.Map(container, options);
+
+        jobData.job.forEach((el) => {
+            new kakao.maps.Marker({
+                map: map,
+                position : new kakao.maps.LatLng(el.lat, el.lng),
+            })
+        })
     }, []);
 
     return (
