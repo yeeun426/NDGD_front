254dc41619e91590c35e283b0b8ad4f2eda9d240
yeeun426 <thsudkcla7@naver.com>
2023-07-18 18:30:37 +0900

    feat : login, signup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,12 @@
 import React,{useEffect, useState} from 'react'
 
 import { useNavigate } from 'react-router-dom';
-import { ModalOverlayStyle, LoginStyle, SnsLoginStyle } from "../styles/LoginStyled";
+import { ModalOverlayStyle, LoginStyle } from "../styles/LoginStyled";
+import axios from "axios";
+
+window.Axios = axios.create({  //서버와 통신할 axios객체 전역으로 선언
+	baseURL : "http://127.0.0.1:8000/"
+})
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
@@ -9,7 +14,6 @@ export default function LoginPage() {
   
   const [emailValid, setEmailValid] = useState(false);
 
-  const [name, setName] = useState('')
   const [phoneNumber, setphoneNumber] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setconfirmPassword] = useState('')
@@ -56,22 +60,67 @@ export default function LoginPage() {
   },[infoCheck, marketingCheck])
 
   const navigate = useNavigate();
-  const [user, setUser] = useState('');
+
   //회원가입
-  const ClickSingup = () => {
-    alert("성공")
+  const ClickSignup = (e) => {
+    if(!infoCheck) {
+      alert("개인정보 수집 및 이용 동의(필수)를 확인해주세요")
+    }
+    else {
+      window.Axios.post("accounts/registration/",{
+        email: email,
+        password1: password,
+        password2: confirmPassword,
+      })
+      .then((res) => {
+        alert("회원가입 성공!");
+        setSignup(false);
+      })
+      .catch((error) => {
+        debugger
+      })
+    }
+  //   const userData = {  //value부분에 user가 입력한 값 들어가야 함(test 데이터임)
+  //     email: email,
+  //     password1: password,
+  //     password2: confirmPassword,
+  // };
+  //   window.Axios.post("accounts/registration/", userData)
+  //   .then(function(response){
+  //     console.log(response);
+  //   });
   }
 
-  // email 확인
-  const onClickEmail = () => {
-    alert("이메일 확인")
+  // 로그인
+  const onClickLogin = (e) => {
+    e.preventDefault();
+
+    window.Axios.post("accounts/login", {
+      email : email,
+      password : password,
+    })
+    .then((res) => {
+      console.log(res)
+      console.log(res.data)
+      alert(email + "님 반갑습니다.")
+      navigate("/", {replace: true});
+    })
+    .catch((error) => {
+      debugger
+    })
+
+  //   alert("이메일 확인")
+  //   const userData = {  //value부분에 user가 입력한 값 들어가야 함(test 데이터임)
+  //     email: "test10@naver.com",
+  //     password: "1q2w3e4r!"
+  // };
+  // window.Axios.post("accounts/login/", userData)
+  //   .then(function(response){
+  //     window.Axios.defaults.headers.common['Authorization'] = `Token ${response.data["key"]}`
+  //     alert(response.data["key"]);  //key잘 넘어오나 test 삭제해도 됨
+  //   });
   };
 
-  //카카오 로그인
-
-  // let sessionStorage = window.sessionStorage;
-  // login
-  
   //이메일 형식
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -138,43 +187,12 @@ export default function LoginPage() {
             />
           </div>
             
-          <button id="emailBtn" onClick={onClickEmail}>로그인</button>
-          <button id="SignupBtn" onClick={() => setSignup(true)}>회원가입</button>
-
-          <div id="modalOr">or</div>
-          <div id="modalNext">다음 계정으로 계속하기</div>
-
-          <SnsLoginStyle>
-            <div className="sns-items">
-            {/* <a href={KAKAO_AUTH_URL}> */}
-              <button id="login-kakao">
-                <svg xmlns="http://www.w3.org/2000/svg" width="22" height="21" viewBox="0 0 22 21"><path fill="#000" fillRule="nonzero" d="M11 0C5.242 0 0 3.823 0 8.539c0 2.932 1.904 5.519 4.804 7.056l-1.22 4.479c-.107.397.343.712.69.483l5.348-3.548c.452.044.91.069 1.377.069 6.076 0 11-3.823 11-8.54 0-4.715-4.924-8.538-11-8.538"></path></svg>
-                <div className="sns-name">Kakao</div>
-              </button>
-            {/* </a> */}
-            </div>
-
-            <div className="sns-items">
-              <button id="login-facebook">
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="23" viewBox="0 0 12 23"><path fill="#fff" fillRule="nonzero" d="M11.214 12.603l.622-4.055h-3.89V5.917c0-1.11.543-2.191 2.285-2.191H12V.274S10.395 0 8.86 0C5.656 0 3.562 1.942 3.562 5.458v3.09H0v4.055h3.562v9.802c.714.112 1.446.17 2.191.17.746 0 1.478-.058 2.192-.17v-9.802h3.269"></path></svg>
-                <div className="sns-name">Facebook</div>
-              </button>
-            </div>
-
-            <div className="sns-items">
-              <button id="login-google">
-                <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23"><g fill="none" fillRule="nonzero"><path fill="#EA4335" d="M11.5 4.574c1.688 0 3.204.58 4.396 1.72l3.299-3.299C17.203 1.14 14.6 0 11.5 0 7.005 0 3.115 2.577 1.223 6.335l3.842 2.98c.905-2.718 3.44-4.741 6.435-4.741z"></path><path fill="#4285F4" d="M22.54 11.761c0-.815-.073-1.6-.21-2.352H11.5v4.448h6.19c-.268 1.438-1.078 2.656-2.296 3.471v2.886h3.717c2.174-2.002 3.429-4.95 3.429-8.453z"></path><path fill="#FBBC05" d="M5.065 13.685c-.23-.69-.36-1.427-.36-2.185s.13-1.495.36-2.185v-2.98H1.223C.444 7.888 0 9.645 0 11.5c0 1.856.444 3.612 1.223 5.165l3.842-2.98z"></path><path fill="#34A853" d="M11.5 23c3.105 0 5.708-1.03 7.61-2.786l-3.716-2.886c-1.03.69-2.347 1.098-3.894 1.098-2.995 0-5.53-2.023-6.435-4.741H1.223v2.98C3.115 20.423 7.005 23 11.5 23z"></path><path d="M0 0L23 0 23 23 0 23z"></path></g></svg>
-                <div className="sns-name">Google</div>
-              </button>
-            </div>
-
-            <div className="sns-items">
-              <button id="login-apple">
-                <svg xmlns="http://www.w3.org/2000/svg" width="19" height="24" viewBox="0 0 19 24"><path fill="#fff" fillRule="nonzero" d="M15.868 12.55c.033 3.574 3.098 4.764 3.132 4.779-.026.084-.49 1.695-1.615 3.36-.972 1.439-1.982 2.872-3.572 2.902-1.562.03-2.065-.938-3.851-.938s-2.344.908-3.823.967c-1.535.059-2.704-1.556-3.684-2.99C.45 17.698-1.08 12.343.975 8.73c1.022-1.795 2.848-2.932 4.83-2.96 1.506-.03 2.929 1.026 3.85 1.026.921 0 2.65-1.27 4.467-1.083.761.032 2.897.31 4.268 2.343-.11.07-2.548 1.506-2.522 4.494m-2.936-8.777c.815-.999 1.363-2.389 1.213-3.772-1.174.048-2.594.792-3.437 1.79-.755.884-1.416 2.298-1.238 3.654 1.31.103 2.647-.673 3.462-1.672"></path></svg>
-                <div className="sns-name">Apple</div>
-              </button>
-            </div>
-          </SnsLoginStyle>
+          <button id="emailBtn" onClick={onClickLogin}>로그인</button>
+          <button id="SignupBtn" onClick={() => {
+            setSignup(true)
+            setEmail("")
+            setPassword("")
+          }}>회원가입</button>
 
           <div className="agree-text">
             걱정마세요! 여러분의 지원 활동은 SNS에 노출되지 않습니다. <br/>
@@ -192,18 +210,21 @@ export default function LoginPage() {
         
         <div className="modal-container">
           <div className="modal-items">
-            <label>이름</label>
+            <label>이메일</label>
             <input 
               type="text" 
-              placeholder="이름을 입력해 주세요." 
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value)
-              }}
+              placeholder="이메일을 입력해 주세요." 
+              value={email}
+              onChange={handleEmail}
             />
+            <div className="error-message">
+              { !emailValid && email.length > 0 && (
+              <div>올바른 이메일 형식을 입력해주세요.</div>
+              )}
+            </div>
           </div>
 
-          <div className="modal-items">
+          {/* <div className="modal-items">
             <label>휴대폰 번호</label>
             <select defaultValue='한국'>
               <option value='한국'>대한민국 +82</option>
@@ -221,7 +242,7 @@ export default function LoginPage() {
                 }}
               />
             </div>
-          </div>
+          </div> */}
 
           <div className="modal-items">
             <label>비밀번호</label>
@@ -263,7 +284,7 @@ export default function LoginPage() {
             <span>이벤트 소식 등 알림 정보 받기</span>
           </div>
 
-          <button id="signupBtn" onClick={()=>{ClickSingup()}}>회원가입하기</button>
+          <button id="signupBtn" onClick={()=>{ClickSignup()}}>회원가입하기</button>
         </div>
       </div>
     </ModalOverlayStyle>
